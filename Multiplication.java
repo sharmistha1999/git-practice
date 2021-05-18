c872684b9a271fa04c8539bf6a69bd62336b1816
sharmistha1999 <84120187+sharmistha1999@users.noreply.github.com>
2021-05-18 20:00:58 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
+System.out.println("enter a ");
+int a=sc.nextInt();
+System.out.println("enter b");
+int b=sc.nextInt();
+System.out.println("multiplication:"+(a*b));
+	}
+
+}
