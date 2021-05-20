8a6c95a7af1f49c76ee3722230ccd188a8289760
sharmistha1999 <84120187+sharmistha1999@users.noreply.github.com>
2021-05-20 18:17:29 +0530

    Add files via upload

---
+++ com/capgemini/abstractclass/FixedDepositMain.java
@@ -0,0 +1,40 @@
+package com.capgemini.abstractclass;
+import java.util.Scanner;
+
+public class FixedDepositMain {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		char ch = 'y';
+		FixedDepositAccount ca = new FixedDepositAccount();
+		ca.openAccount();
+		while (ch == 'y') {
+			System.out.println("Enter the choice");
+			System.out.println("viewAcc");
+			System.out.println("addMoney");
+			System.out.println("withDrawMoney");
+			System.out.println("CheckBal");
+			String option = sc.next();
+			switch (option) {
+			case "viewAcc": 
+				ca.viewAccount();
+				break;
+			case "addMoney":
+				ca.addMoney();
+				break;
+			case "withDrawMoney":
+				ca.withDraw();
+				break;
+			case "CheckBal":
+				ca.checkBalance();
+				break;
+			default:
+				System.out.println("Not valid option");
+
+			}// switch close
+			System.out.println("do you want to continue");
+			ch = sc.next().trim().charAt(0);
+		} // while close
+	}
+
+}
